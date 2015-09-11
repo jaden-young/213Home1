9d35a938eedbf7fb8cf818c2cdd29e2e526e1eba
jaden-young <jaden.young44@gmail.com>
2015-09-11 00:40:04 -0500

    Initial commit

---
+++ Exercise1/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    public class Student
+    {
+        private string name;
+        private string idNumber;
+        private string 
+    }
+}
