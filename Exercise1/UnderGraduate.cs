add7b7dcd67839b1755116a522865646a1c88e63
jaden-young <jaden.young44@gmail.com>
2015-09-11 01:29:52 -0500

    Undergraduate class

---
+++ Exercise1/UnderGraduate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    public class UnderGraduate : Student
+    {
+        private string name;
+        private string classification;
+        private string[] classifications = {
+            "Freshman",
+            "Sophomore",
+            "Junior",
+            "Senior"
+        };
+        private string guardianName;
+        private string guardianAddress;
+
+        public string Classification
+        {
+            get;
+            set
+            {
+                foreach (string gradeYear in classifications)
+                {
+                    if (gradeYear.Equals(value, StringComparison.OrdinalIgnoreCase))
+                    {
+                        classification = value;
+                        return;
+                    }
+                }
+            }
+        }
+
+        public string guardianName
+        {
+            get;
+            set;
+        }
+
+        public string guardianAddress
+        {
+            get;
+            set;
+        }
+    }
+}
